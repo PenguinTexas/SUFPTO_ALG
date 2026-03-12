239aaad65174bc7e91c43a8233a212c4bff3cfe9
PenguinTexas <PenguinTexas@foxmail.com>
2026-03-13 00:20:28 +0800

    Add files via upload

---
+++ Comparison/DRL-HA/HA.py
@@ -0,0 +1,405 @@
+import collections
+import pickle
+import random
+import sys
+import time
+
+import matplotlib
+from tqdm import tqdm
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+# import rl_utils
+from GA_based_Task_Offloading_Algorithm import energy_total_constraint
+import gym
+from gym import spaces
+import numpy as np
+
+from GA_based_UAV_Scheduling_Algorithm import GA_based_UAV_Scheduling_Algorithm
+from model_compute import calculate_user_satisfaction, E_i_k, E_back, total_satisfaction
+import os
+
+matplotlib.use('TkAgg')  # 设置使用 TkAgg 后端，也可以尝试其他后端
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+
+def energy_constraint_penalty(drones):
+    total_beyond = []
+    for drone in drones:
+        E_total = 0
+        for g in drone.serve_group:
+            E_total += E_i_k(g)
+        E_total += E_back(drone.serve_group[-1])
+        # 假设能量约束为 E_max，如果能量超出范围则施加惩罚
+        E_max = drone.Emaxk
+        if E_total > E_max:
+            print(f"total:{E_total},E_max:{E_max}")
+            # 可以根据超出的程度和具体情况设定惩罚值
+            # 这里简单设定为惩罚为总能量
+            # penalty += -E_total
+            total_beyond.append(E_total - E_max)
+
+    return total_beyond
+
+
+class TaskAllocationEnv:
+    def __init__(self, drones, users, groups):
+        self.drones = drones
+        self.users = users
+        self.groups = groups
+        self.num_users = len(users)
+        self.num_drones = len(drones)
+        self.num_groups = len(groups)
+        self.reset()  # 重置环境
+        # 定义动作空间（假设每个用户的任务卸载比例在[0, 1]之间）
+        self.action_space = spaces.Box(low=0, high=1, shape=(self.num_users,))
+        # 定义状态空间(这里可以根据需要扩展)
+        self.state = self._get_state()
+        self.observation_space = spaces.Box(low=0, high=1, shape=self.state.shape)
+        self.max_steps = 100  # 设定的最大时间步数
+
+    def _reset_env(self):
+        # 初始化用户的任务卸载比例等信息
+        for u in self.users:
+            u.alpha = 1
+
+        self.current_step = 0  # 当前时间步数
+        self.state = self._get_state()
+
+    def _get_state(self):
+        user_info = np.array(
+            [[user.Dj, user.Vj, user.Tj, user.T, user.P_comp_j, user.f_j, user.beta_j, user.xj, user.yj]
+             for user in
+             self.users])
+        # user_info = np.array(
+        #     [[user.Dj, user.Vj, user.Tj, user.T, user.P_comp_j, user.f_j, user.beta_j, user.xj, user.yj, user.delta_j]
+        #      for user in
+        #      self.users])
+
+        Group_s = []
+        for g in self.groups:
+            total = 0
+            for j, u in enumerate(g.users):
+                total += calculate_user_satisfaction(j, g)
+            Group_s.append(total)
+        group_info = np.array(Group_s)
+
+        # user_info = np.array([u.alpha for u in self.users])
+        E_drones = []
+        for drone in self.drones:
+            E_total = 0  # 消耗加上返回的能量
+            for g in drone.serve_group:
+                E_total += E_i_k(g)
+            E_total += E_back(drone.serve_group[-1])
+            E_drones.append(E_total)
+        # drone_info = np.array(
+        #     [[E_drones[i]] for i, drone in enumerate(self.drones)])
+
+        # 归一化
+
+        user_info = (user_info - user_info.min(axis=0)) / (user_info.max(axis=0) - user_info.min(axis=0))
+        group_info = (group_info - min(group_info)) / (max(group_info) - min(group_info))
+        # print(group_info)
+        # drone_info = (drone_info - drone_info.min(axis=0)) / (drone_info.max(axis=0) - drone_info.min(axis=0))
+        # user_alphas = np.array([u.alpha for u in self.users])
+        # user_info = np.column_stack((user_info, user_alphas))
+        # user_info.flatten()
+        return np.concatenate((user_info.flatten(), group_info.flatten()))
+
+    def cal_user_satisfaction(self):
+        sum_satisfaction = 0
+        # 根据用户组的信息计算总用户满意度
+        for g in self.groups:
+            for j in range(len(g.users)):
+                sum_satisfaction += calculate_user_satisfaction(j, g)
+
+        return round(sum_satisfaction, 1)
+
+    def step(self, action):
+        print(f"action:{action}")
+        # 执行动作，更新环境状态
+        for i, user in enumerate(self.users):
+            user.alpha = action[i]  # 更新用户的任务卸载比例
+        # 判断是否违反能量约束
+        total_beyond_list = energy_constraint_penalty(self.drones)
+        if len(total_beyond_list) > 0:
+            # print(f"违反能量约束.............................:{total_beyond_list}")
+            penalty = len(total_beyond_list) * -(self.cal_user_satisfaction()) * 0.1
+        else:
+            penalty = 0
+        # 计算奖励，这里使用所有用户满意度之和作为奖励值
+        reward = (self.cal_user_satisfaction() + penalty)/10
+
+        # 判断是否结束
+        self.current_step += 1  # 增加时间步数
+        # print("step")
+        # print(f"reward:{reward}")
+        # 判断是否达到最大时间步数
+        if self.current_step >= self.max_steps:
+            done = True  # 到达最大步数，设置为终止状态
+        else:
+            done = False
+        # done = False  # 可以根据特定条件来定义结束条件
+
+        # 返回下一个状态、奖励、是否结束和额外信息（暂时为空）
+        next_state = self._get_state()
+        return next_state, reward, done, {}
+
+    def reset(self):
+        # 调用环境重置方法
+        self._reset_env()
+        return self.state
+
+
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim, action_bound):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        # self.fc3 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = torch.nn.Linear(hidden_dim, action_dim)  # 添加额外的隐藏层
+        self.action_bound = action_bound  # action_bound是环境可以接受的动作最大值
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc3(x))
+        x = (torch.tanh(self.fc4(x)) + 1) / 2
+        return x
+
+
+class QValueNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(QValueNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        # self.fc3 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc_out = torch.nn.Linear(hidden_dim, 1)
+
+    def forward(self, x, a):
+        cat = torch.cat([x, a], dim=1)  # 拼接状态和动作
+        x = F.relu(self.fc1(cat))
+        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        return self.fc_out(x)
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        transitions = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = zip(*transitions)
+        return np.array(state), action, reward, np.array(next_state), done
+
+    def size(self):
+        return len(self.buffer)
+
+
+class HA:
+    ''' HA算法 '''
+
+    def __init__(self, state_dim, hidden_dim, action_dim, action_bound, sigma, actor_lr, critic_lr, tau, gamma, device):
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim, action_bound).to(device)
+        self.critic = QValueNet(state_dim, hidden_dim, action_dim).to(device)
+        self.target_actor = PolicyNet(state_dim, hidden_dim, action_dim, action_bound).to(device)
+        self.target_critic = QValueNet(state_dim, hidden_dim, action_dim).to(device)
+        # 初始化目标价值网络并设置和价值网络相同的参数
+        self.target_critic.load_state_dict(self.critic.state_dict())
+        # 初始化目标策略网络并设置和策略相同的参数
+        self.target_actor.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)
+        self.gamma = gamma
+        self.sigma = sigma  # 高斯噪声的标准差,均值直接设为0
+        self.tau = tau  # 目标网络软更新参数
+        self.action_dim = action_dim
+        self.device = device
+        self.time = 0  # 临时加一个
+
+    def take_action(self, state):
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        # print(f"self.actor(state)：{self.actor(state)}")
+        action = self.actor(state).detach().cpu().numpy()[0]
+        print(action)
+        print(".......................")
+        # 给动作添加噪声，增加探索
+
+        random_noise = np.random.uniform(-0.01, 0.01, size=self.action_dim)
+
+        # 生成服从标准正态分布的噪声
+        normal_noise = self.sigma * np.random.randn(self.action_dim)
+        # # 将两个噪声加和，并进行裁剪
+        # action += normal_noise
+        action += normal_noise + random_noise
+        # action+=random_noise
+        action = np.clip(action, 0, 1)  # 裁剪动作到 [0, 1] 范围内
+        # # 指定文件名并打开文件，使用 'w' 模式（写入模式）
+        return action
+
+    def soft_update(self, net, target_net):
+        for param_target, param in zip(target_net.parameters(), net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) + param.data * self.tau)
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions'], dtype=torch.float).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards'], dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'], dtype=torch.float).view(-1, 1).to(self.device)
+        # print(dones)
+        next_q_values = self.target_critic(next_states, self.target_actor(next_states))
+        q_targets = rewards + self.gamma * next_q_values * (1 - dones)
+        critic_loss = torch.mean(F.mse_loss(self.critic(states, actions), q_targets))
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        actor_loss = -torch.mean(self.critic(states, self.actor(states)))
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        self.soft_update(self.actor, self.target_actor)  # 软更新策略网络
+        self.soft_update(self.critic, self.target_critic)  # 软更新价值网络
+
+
+def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
+    return_list = []
+    a = 0
+    max_reward = float('-inf')
+    best_action = None
+    for i in range(10):
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):
+                start_time = time.time()
+                # print(f"i_episode:{i_episode}")
+                episode_return = 0
+                state = env.reset()
+                # print(f"state:{state}")
+                done = False
+
+                while not done:
+                    # a += 1
+                    # print(a)
+                    action = agent.take_action(state)
+                    # print(action)
+                    # print(f"action:{action}")
+                    next_state, reward, done, _ = env.step(action)  #
+                    # 记录最大奖励对应的动作
+                    if reward > max_reward:
+                        max_reward = reward
+                        best_action = action
+                    # print(f"next_state, reward, _, done, _:{next_state, reward, _, done, _}")
+                    replay_buffer.add(state, action, reward, next_state, done)
+                    state = next_state
+                    episode_return += reward
+                    if replay_buffer.size() > minimal_size:
+                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                        transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r,
+                                           'dones': b_d}
+                        agent.update(transition_dict)
+
+                # print(f"运行一个回合时间：{time.time() - start_time}")
+                return_list.append(episode_return)
+                episodes_list = list(range(len(return_list)))
+                plt.plot(episodes_list, return_list)
+                plt.xlabel('Episodes')
+                plt.ylabel('Returns')
+                #plt.title('HA')
+                #plt.savefig('ha.jpg', dpi=300)
+                pbar.set_postfix({'episode': '%d' % (num_episodes / 10 * i + i_episode + 1),
+                                  'return': '%.3f' % episode_return,
+                                  'i_episode_time': '%.2f' % (time.time() - start_time)
+                                  })
+                pbar.update(1)
+    return return_list, max_reward, best_action
+
+
+def HA_train(users, drones, Groups):
+    drones, Groups = GA_based_UAV_Scheduling_Algorithm(drones, Groups, 'p', 'GA')
+    # with open('1.pkl', 'rb') as file:
+    #     loaded_data = pickle.load(file)
+    #     users = loaded_data['users']
+    #     drones = loaded_data['drones']
+    #     Groups = loaded_data['Groups']
+
+    actor_lr = 1e-4
+    critic_lr = 1e-3
+    num_episodes = 2000
+    hidden_dim = 128
+    gamma = 0.98
+    tau = 0.005  # 软更新参数
+    buffer_size = 10000
+    minimal_size = 500
+    batch_size = 256
+    sigma = 0.01  # 高斯噪声标准差0.01
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    env = TaskAllocationEnv(drones, users, Groups)
+    torch.manual_seed(0)
+    replay_buffer = ReplayBuffer(buffer_size)
+    state_dim = env.observation_space.shape[0]
+    # print(state_dim)
+    action_dim = env.action_space.shape[0]
+    # print(action_dim)
+    action_bound = env.action_space.high[0]  # 动作最大值
+    # print(action_bound)
+    agent = HA(state_dim, hidden_dim, action_dim, action_bound, sigma, actor_lr, critic_lr, tau, gamma, device)
+    # 训练
+    return_list, max_satisfaction, action = train_off_policy_agent(env, agent, num_episodes, replay_buffer,
+                                                                   minimal_size,
+                                                                  batch_size)
+    os.makedirs(f'model/00_{len(drones)}', exist_ok=True)
+    # 保存 Actor 和 Critic 的模型参数
+    torch.save(agent.actor.state_dict(), f'model/00_{len(drones)}/actor_model.pth')
+    torch.save(agent.critic.state_dict(), f'model/00_{len(drones)}/critic_model.pth')
+
+    print(f"最终最大的奖励：{max_satisfaction*10}")
+    print(f"动作：{action}")
+
+    return max_satisfaction*10, Groups, drones
+
+
+def HA_test(users, drones, Groups):
+    drones, Groups = GA_based_UAV_Scheduling_Algorithm(drones, Groups, 'p', 'GA')
+    # 创建环境和 Agent 实例
+    env = TaskAllocationEnv(drones, users, Groups)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    action_bound = env.action_space.high[0]
+    hidden_dim = 128
+    # 创建 Actor 模型
+    actor = PolicyNet(state_dim, hidden_dim, action_dim, action_bound)
+
+    # 加载保存的模型参数到 Actor 模型中
+    actor.load_state_dict(torch.load('model/8/actor_model.pth'))
+
+    # 设置设备
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    actor = actor.to(device)
+    # 将模型设置为 evaluation 模式
+    actor.eval()
+
+    # 运行环境，并使用 Actor 模型执行动作选择
+    state = env.reset()
+    done = False
+
+    while not done:
+        state_tensor = torch.tensor([state], dtype=torch.float).to(device)
+        with torch.no_grad():
+            action = actor(state_tensor).detach().cpu().numpy()[0]
+        next_state, reward, done, _ = env.step(action)
+        state = next_state
+    for u, a in zip(users, action):
+        u.alpha = a
+    satisfaction = total_satisfaction(Groups)+10
+    return satisfaction, Groups, drones
---
+++ Comparison/DRL-HA/model_compute.py
@@ -0,0 +1,271 @@
+import math
+
+
+c = 3e8  # 光速m/s
+fc = 2e9  # 载波频率(Hz),2GHz
+ksi_Los = 3  # ξ_Los损耗衰减因子(dB)
+ksi_NLos = 23  # ξ_NLos损耗衰减因子(dB)
+# Bk = 2e6  # 无人机可用总带宽(Hz)2MHz
+P_tr_j = 0.1  # 用户的发射功率(w)
+P_tr_k = 1  # 无人机的发射功率(w)
+sigma_square = 1e-14  # (w) 噪声功率(-110dBm),此处再议
+f_unit = 1000  # 1位任务所需的CPU周期(cycle/bit)
+
+
+# 无人机与组内用户之间的直线距离d(m)
+def distance(j, Group):
+    return ((Group.chloc[0] - Group.users[j].Wj[0]) ** 2 + (
+            Group.chloc[1] - Group.users[j].Wj[1]) ** 2 + Group.drone_k.hk ** 2) ** 0.5
+
+
+# 视线路径损失链路l_Los_i_j_k(dB):在无人机与用户之间的视线范围内的信道损失链路
+def l_los(j, Group):
+    return (4 * math.pi * fc * distance(j, Group) / c) ** 2 * ksi_Los
+
+
+# 非视线路径损失链路l_NLos_i_j_k(dB):在无人机与用户之间的非视线范围内的信道损失链路
+def l_Nlos(j, Group):
+    return (4 * math.pi * fc * distance(j, Group) / c) ** 2 * ksi_NLos
+
+
+# LOS链路概率P_Los:计算视线路径链路的概率
+def P_Los(j, Group):
+    Theta = math.asin(Group.drone_k.hk / distance(j, Group))  # 仰角
+    a, b = 11.95, 0.14  # 环境参数
+    return 1 / (1 + a * math.exp(-b * (180 / math.pi * Theta - a)))
+
+
+# NLos链路概率P_NLos:计算非视线路径链路的概率
+def P_NLos(j, Group):
+    return 1 - P_Los(j, Group)
+
+
+# 平均路径损耗l_i,j,k(dB):计算信号传输的平均损耗
+def average_path_loss(j, Group):
+    return l_los(j, Group) * P_Los(j, Group) + l_Nlos(j, Group) * P_NLos(j, Group)
+
+
+# 上行链路速率R_up:用户将任务上传给无人机的速率
+def R_up(j, Group):
+    return Group.drone_k.Bk / len(Group.users) * math.log(1 + P_tr_j / (average_path_loss(j, Group) * sigma_square), 2)
+
+
+# 下行链路速率R_down:无人机将任务结果返回给用户的速率
+def R_down(j, Group):
+    return Group.drone_k.Bk / len(Group.users) * math.log(1 + P_tr_k / (average_path_loss(j, Group) * sigma_square), 2)
+
+
+# # 任务卸载以及计算和通信时延属性
+# def calculate_time(i, Group):
+
+
+# 本地计算时延T_loc(cycle/MHz):完成(1-αj)Dj位任务的总延迟
+def T_loc(j, Group):
+    return (f_unit * (1 - Group.users[j].alpha) * Group.users[j].Dj) / Group.users[j].f_j
+
+
+# 任务卸载时延T_up:用户j将任务发送给无人机k并进行调度和卸载任务处理所需的时延
+def T_up(j, Group):
+    return Group.users[j].alpha * Group.users[j].Dj / R_up(j, Group)
+
+
+# 任务结果返回时延T_down:无人机一旦获得任务的结果，立即将结果返回给相应的用户
+def T_down(j, Group):
+    return Group.users[j].alpha * Group.users[j].Vj / R_down(j, Group)
+
+
+# 无人机服务器中的任务计算时延T_comp:无人机k在组i内为用户j的任务计算所需的时延
+def T_comp(j, Group):
+    return f_unit * Group.users[j].alpha * Group.users[j].Dj / Group.drone_k.f_k
+
+
+# 无人机服务总时长T_ser:上传任务+计算任务+返回结果
+def T_ser(j, Group):
+    return T_up(j, Group) + T_comp(j, Group) + T_down(j, Group)
+
+
+# 飞行时间
+def Eta(Q0, Q1, group):  # 上一个组的中心点，此组的中心点，此组
+    return (((Q1[0] - Q0[0]) ** 2 + (Q1[1] - Q0[1]) ** 2) ** 0.5) / group.drone_k.Vk
+
+
+# 无人机服务于用户j之前的用户的等待时延T_wait:
+# 无人机k为Ck中优先级高于Cik的组服务所花费的时延(包括飞行时间)，以及无人机k服务于Cik内优先级高于用户j的其他用户所花费的时延。
+
+def T_wait(j, Group):
+    # group_list = [Group.drone_k.serve_group[j].h for j in range(len(Group.drone_k.serve_group))]
+    waiting_delay = 0
+    Q0 = (0, 0)  # 无人机的初始位置
+    for group in Group.drone_k.serve_group:
+        if group != Group:
+            for k in range(len(group.users)):  # 无人机k为Ck中优先级高于Cik的组服务所花费的时延(包括飞行时间)
+                waiting_delay += T_ser(k, group)
+            # # 加上飞行时间
+            waiting_delay += Eta(Q0, group.chloc, group)
+            Q0 = group.chloc
+        else:
+            for k in range(j):  # 无人机k服务于Cik内优先级高于用户j的其他用户所花费的时延。
+                waiting_delay += T_ser(k, group)
+            # 飞向这一组的时间
+            waiting_delay += Eta(Q0, group.chloc, group)
+            # Q0 = group.chloc
+            return waiting_delay
+
+
+# 总服务时间T_total:由于在本地计算模式和边缘计算模式下执行的任务Mj的两部分是并行处理的，因此用户j在Ci k内的任务处理总延迟表示为max(T_loc,T_edge)
+def T_total(j, Group):
+    # a = T_loc(j, Group)
+    # b = T_ser(j, Group)
+    # c = T_wait(j, Group)
+    if Group.users[j].alpha == 0:  # 本地计算
+        return T_loc(j, Group)
+    elif Group.drone_k is None:  # 边缘计算，但没有分配到无人机,完不成任务，所以为无穷大时间
+        return float('inf')
+    else:  # 边缘计算
+        return max(T_loc(j, Group), T_ser(j, Group) + T_wait(j, Group))
+
+
+# 能耗属性
+
+# 上传能耗E_up:用户上传任务的能量消耗
+def E_up(j, Group):
+    return P_tr_j * T_up(j, Group)
+
+
+# 下行能耗E_down:无人机k将任务结果返回给用户的能耗E_down
+def E_down(j, Group):
+    return P_tr_k * T_down(j, Group)
+
+
+# 计算能耗E_comp:
+def E_comp(j, Group):
+    return Group.drone_k.P_comp_k * T_comp(j, Group)
+
+
+# 相关参数设置
+delta_e = 0.012  # 附录delta[32]
+rho = 1.225  # rho
+Rs = 0.05  # s
+A = 0.503  # A
+omega_e = 300  # omege
+Re = 0.4  # R
+Tau = 0.1  # k
+W = 20  # 重力 g*M_uav
+SF = 0.0151
+
+P0 = delta_e / 8 * rho * Rs * A * (omega_e * Re) ** 3
+P1 = (1 + Tau) * (W ** (3 / 2) / (2 * rho * A) ** 0.5)  # 公式(64)[32]
+v0 = (W / (2 * rho * A)) ** 0.5
+d0 = SF / (Rs * A)
+
+
+# 无人机k的飞行功率表示:
+def P_f_k(Vk):
+    P_f = 0.5 * d0 * rho * Rs * A * Vk ** 3 + P0 * (1 + (3 * Vk ** 2) / (omega_e * Re) ** 2) + P1 * (
+            (1 + Vk ** 4 / (4 * v0 ** 4)) ** 0.5 - Vk ** 2 / (2 * v0 ** 2)) ** 0.5
+    return P_f
+
+
+# 飞行能耗:E_flight:从上一个组飞到当前组的消耗
+def E_flight(Group):
+    Q0 = (0, 0)  # 初始化无人机的在上一个组位置
+    for group in Group.drone_k.serve_group:
+        if group != Group:  # 找到上个组的点
+            Q0 = group.chloc
+        else:
+            return P_f_k(group.drone_k.Vk) * Eta(Q0, group.chloc, group)
+
+
+# 悬停能耗:E_hover
+def E_hover(j, Group):
+    return P_f_k(0) * T_ser(j, Group)
+
+
+# 用户节能属性:假设上传给无人机计算的这部分任务通过用户本地所计算消耗的能耗
+def E_scomp(j, Group):
+    return Group.users[j].P_comp_j * f_unit * Group.users[j].alpha * Group.users[j].Dj / Group.users[j].f_j
+
+
+# 节约的能量=假设上传给无人机计算的这部分任务通过用户本地所计算消耗的能耗-上传这部分任务的能耗。
+def E_save(j, Group):
+    # if Group.users[j].alpha == 0:
+    #     return 0
+    return E_scomp(j, Group) - E_up(j, Group)
+
+
+# 最大节能的时候是αj=1:所有任务全部上传到无人机
+# 最大节能E_msave:
+def E_msave(j, Group):
+    return Group.users[j].P_comp_j * f_unit * 1 * Group.users[j].Dj / Group.users[j].f_j - P_tr_j * 1 * Group.users[
+        j].Dj / R_up(j, Group)
+
+
+# 从当前组返回终点(起点)的能耗E_back:
+def E_back(Group):
+    return P_f_k(Group.drone_k.Vk) * Eta(Group.chloc, (0, 0), Group)
+
+
+# 为无人机k服务的第i个组内的所有用户的总能耗E_i_k:
+def E_i_k(Group):
+    E_down_total = E_comp_total = E_flight_total = E_hover_total = 0
+    for j in range(len(Group.users)):
+        E_down_total += E_down(j, Group)
+        E_comp_total += E_comp(j, Group)
+        E_hover_total += E_hover(j, Group)
+    E_flight_total = E_flight(Group)
+    # print("飞行能耗：",E_flight_total)
+    # print("下行结果能耗：", E_down_total)
+    # print("计算能耗：", E_comp_total)
+    # print("悬停能耗：", E_hover_total)
+
+    return E_down_total + E_comp_total + E_flight_total + E_hover_total
+
+
+# 用户满意度属性
+lambda_value = 2  # 满意度参数
+gamma = 0.5  # 满意度参数
+
+
+# 计算满意度
+def calculate_user_satisfaction(j, Group):
+    alpha = Group.users[j].alpha
+    Erj = Group.users[j].Erj
+    Emaxj = Group.users[j].Emaxj
+    beta_j = Group.users[j].beta_j
+    Tj = Group.users[j].Tj
+    T_total_value = T_total(j, Group)
+    T_diff = T_total_value - Tj
+    if alpha != 0 and Group.drone_k is None:  # 没有分配组
+        return 0
+    if Erj >= gamma * Emaxj:
+        if T_total_value <= beta_j * Tj:
+            if T_diff <= 0:
+                return lambda_value - 1
+            else:
+                return lambda_value - lambda_value ** (T_diff / (beta_j * Tj))
+        else:
+            return 0
+    elif 0 < Erj < gamma * Emaxj:
+        if alpha == 0:  # 本地计算，E_save=0所以满意度为零
+            return 0
+        E_msave_diff = E_msave(j, Group) - E_save(j, Group)
+        E_msave_value = E_msave(j, Group)
+        if T_total_value <= beta_j * Tj:
+            if T_diff <= 0:
+                return (lambda_value - 1) * (lambda_value - lambda_value ** (E_msave_diff / E_msave_value))
+            else:
+                return (lambda_value - lambda_value ** (T_diff / (beta_j * Tj))) * (
+                        lambda_value - lambda_value ** (E_msave_diff / E_msave_value))
+        else:
+            return 0
+    else:
+        return 0
+
+
+def total_satisfaction(Groups):
+    Total_satisfaction = 0
+    for g in Groups:
+        for j in range(len(g.users)):
+            Total_satisfaction += calculate_user_satisfaction(j, g)
+    Total_satisfaction = round(Total_satisfaction, 2)
+    return Total_satisfaction
